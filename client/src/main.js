362a779e071549f0f2bd16bcb8785bb22ff867b8
Muhammad Khairul Rizal Purwanto <rizal.purwanto.id@gmail.com>
2022-12-18 10:40:34 +0700

    URL

--- client/src/main.js
+++ client/src/main.js
@@ -8,7 +8,8 @@ import "tw-elements";
 import VueSocketIOExt from 'vue-socket.io-extended';
 import { io } from 'socket.io-client';
 
-const socket = io('https://iproject2.herokuapp.com');
+//const socket = io('https://iproject2.herokuapp.com');
+const socket = io('https://p2-iproject-server-1-production.up.railway.app')
 
 Vue.use(VueSocketIOExt, socket, {store});
 
